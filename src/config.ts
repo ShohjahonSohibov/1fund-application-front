6558e23478ffb375dec05cde42857ba217c8bef3
ShohjahonSohibov <shohjahonsohibov0@gmail.com>
2025-11-22 13:05:02 +0500

    i

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,5 @@
-export const API_URL = "/api/send";
+export const API_URL =
+  (import.meta as any)?.env?.VITE_API_URL ||
+  ((import.meta as any)?.env?.DEV
+    ? "/api/send"
+    : "https://onefund-application-receiver-bot.onrender.com/api/send");
